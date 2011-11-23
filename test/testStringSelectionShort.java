960c15582c1e23a48c8f7fc031e81983b4897f55
Patrick Menezes <patrickwds1@gmail.com>
2011-11-22 23:52:08 -0200

    Update test/testStringSelectionShort.java

--- test/testStringSelectionShort.java
+++ test/testStringSelectionShort.java
@@ -6,24 +6,31 @@ import org.junit.Test;
 public class testStringSelectionShort {
 
 	@Test
-	public void deveOrdenarTest() {
-		String linhas = stringSelectionShort.selectionSort("Eu odeio os jabutis");
-		assertEquals("Eu os odeio jabutis",linhas);
+	public void manipularStringDeRatoRoeu() {
+		String fraseFormatada = stringSelectionShort.selectionSort("O rato roeu a roupa do rei de roma");
+		assertEquals("O a do de rei roeu rato roma roupa", fraseFormatada);
+	}
 
-}
-	
 	@Test
-	public void deveOrdenarTest2() {
-		String linhas = stringSelectionShort.selectionSort("O rato roeu a roupa do rei de roma");
-		assertEquals("O a do de rei rato roeu roma roupa",linhas);
+	public void manipularStringDeCegonhas() {
+		String fraseFormatada = stringSelectionShort.selectionSort("Um pequeno jabuti xereta viu dez cegonhas felizes.");
+		assertEquals("Um viu dez xereta jabuti pequeno cegonhas felizes.", fraseFormatada);
+	}
 
-}
-	
 	@Test
-	public void deveOrdenarTest3() {
-		String linhas = stringSelectionShort.selectionSort("h hhhh hhhh h hhhhh hh hhh hh hhhh");
-		assertEquals("h h hh hh hhh hhhh hhhh hhhh hhhhh",linhas);
+	public void manipularStringDoJoao() {
+		String fraseFormatada = stringSelectionShort.selectionSort("A Maria compro pao na casa do Joao.");
+		assertEquals("A na do pao casa Maria Joao. compro", fraseFormatada);
+	}
 
-}
-
+	@Test
+	public void manipularStringDoComecoGrande() {
+		String fraseFormatada = stringSelectionShort.selectionSort("Carmelita Maria compro pao na casa do Joao.");
+		assertEquals("na do pao casa Maria Joao. compro Carmelita", fraseFormatada);
+	}
+	@Test
+	public void manipularStringDoFinalMenor() {
+		String fraseFormatada = stringSelectionShort.selectionSort("Eu odeio os Jabutis malditos ha .");
+		assertEquals(". os ha Eu odeio Jabutis malditos", fraseFormatada);
+	}
 }
