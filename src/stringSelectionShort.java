c7da685e0d864be5dce5127957d270d031e1b842
Patrick Menezes <patrickwds1@gmail.com>
2011-11-26 20:59:05 -0200

    Update src/stringSelectionShort.java

--- src/stringSelectionShort.java
+++ src/stringSelectionShort.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-/**
+/**Classe com a função de ordenar de forma crescente as palavras da frase digitada pelo usuário. 
  *
  * @author Douglas Lenfers
  * @author Patrick Menezes
@@ -28,14 +28,27 @@ public class stringSelectionShort {
 		public static String[] separaFrase(String frase) {
 			return frase.split(" ");
 		}
-/**
+/**Método responsavel pela ondenação das palavras e sua montagem em frase novamente.
  * 
+ * No inicio o array "palavra" recebe em suas posições cada palavra que foi separada no 
+ * método anterior, o primeiro "for" é responsável por varrer todas as posições de 
+ * "palavra" começando na primeira posição, o segundo "for" tem como inicio a segunda posição 
+ * e ambos tem como fim o numero de palavras, dentro do segundo "for" temos uma condição, 
+ * que compara a primeira palavra com a segunda, se a condição não for atendida o o segundo 
+ * "for" implementa mais uma posição comparando a primeira palavra com a terceira e assim 
+ * por diante até encontrar uma menor que a palavra inicial, quando isto ocorre a uma troca o 
+ * int "menor" recebe o valor de "j", saindo do segundo "for". Ficando então somente dentro
+ * do primeiro "for" que também tem uma condição, se "menor" for diferente do índice "i" á 
+ * uma troca, a posição no array da palavra inicial fica com o valor da palavra posterior 
+ * considerada menor e o mesmo ocorre com a palavra posterior fica com o valor da palavra inicial
+ * com o auxilio de uma variável temporária a String "temp". 
+ *
  * 
  * @see separaFrase(frase) 	Associação entre o metodo "selectionSort" e "separaFrase".
  * @param frase 		Parametro de entrada de dados.
- * @param palavra 		Array para armazenamento dos dados vindos de "separaFrase".
+ * @param palavra 		Írray para armazenamento dos dados vindos de "separaFrase".
  * @param i 			Indice do array.
- * @param j 			Indice do array mais uma posição.
+ * @param j 			Índice do array mais uma posição.
  * @param menor 		Recebe um indice.
  * @param palavraPosterior	Array palavra tendo como indice "j".
  * @param palavraAtual		Array palavra tendo como indice "menor".
